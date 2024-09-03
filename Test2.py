c0a41cb2375c126f2cdd26169fe1b3aaf9d2dcee
pranay-raj-goud <pranay@convegenius.ai>
2024-09-03 13:15:29 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -224,7 +224,8 @@ def main():
     # Get the corresponding parameter key
             #selected_param = list(parameter_descriptions.keys())[parameter_options.index(selected_description)]
             #st.write(parameter_descriptions[selected_param])
-            st.markdown("X"*LEN(partner_id))
+            Pranay = Len(partner_id)
+            st.markdown("X"*Pranay)
             
 
     # Add notification messages
