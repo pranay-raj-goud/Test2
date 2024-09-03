84b58c4e6ee3307522fe14aff5b272b15a5ba6f8
pranay-raj-goud <pranay@convegenius.ai>
2024-09-03 20:50:24 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -241,7 +241,7 @@ def main():
             # Warning box in yellow color
             st.markdown(
                 """
-                <div style='background-color: skyblue; padding: 5px; border-radius: 1px;'>
+                <div style='background-color: navyblue; padding: 5px; border-radius: 1px;'>
                 <p><span style='color: red; font-weight: bold;'>Note:</span> <span style='color: black;'>Avoid Digit Overload in your Enrolments</span></p>
                 </div>
                 """,
